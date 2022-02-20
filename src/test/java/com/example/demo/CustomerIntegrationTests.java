849d5e1df9bf2da3daf53cf25d30bfbcfdbf005a
ch <mkmsacc@outlook.com>
2022-02-20 22:22:34 +0700

    Add CustomerIntegrationTests

---
+++ src/test/java/com/example/demo/CustomerIntegrationTests.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+class CustomerIntegrationTests {
+
+
+    @Container
+    private static final MySQLContainer<?> container = new MySQLContainer<>("mysql:latest");
+
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Test
+    void shouldHaveADefaultDatabase() {
+        var customers = customerDao.findAll();
+        Assertions.assertThat(customers).hasSize(2);
+    }
+}
