c1901ec618ad7ad89ba8d4bbb21cfef2809da319
ch <mkmsacc@outlook.com>
2022-02-20 19:49:19 +0700

    Add CustomController

---
+++ src/main/java/com/example/demo/CustomController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+class CustomController {
+    private final CustomerDao customerDao;
+
+    @GetMapping
+    public List<Customer> customers() {
+        var customers = customerDao.findAll();
+        customers.forEach(customer -> log.info("Found a customer: {}", customer));
+        return customers;
+    }
+}
