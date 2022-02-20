ce0b0caaf3a39098d67b29be379cf1af55e47fb8
ch <mkmsacc@outlook.com>
2022-02-20 19:48:46 +0700

    Add CustomerDao

---
+++ src/main/java/com/example/demo/CustomerDao.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+class CustomerDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<Customer> findAll() {
+        return jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers",
+                (rs, rowNum) -> new Customer(rs.getLong("id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"))
+        );
+    }
+}
